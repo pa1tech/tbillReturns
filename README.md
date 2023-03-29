9439442d2822f6aa7301ebd73864a7ba8493ab59
Pavan Vemuri <pavanvemuri411@gmail.com>
2023-03-29 22:07:20 +0530

    new

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# TBill Returns History
+---
+title: T-Bill Returns History
+
+image:
+  path: https://pa1tech.github.io/logo.jpg
+---
+
+# T-Bill Returns History
+
 Python script to scrape T-Bill Returns Data of the year from RBI press releases
 
-History of the returns is exported in excel format
-
-Requriements: selenium, pandas, webdriver_manager
+* History of the returns exported in excel format
+* Human intervention needed if challenged it Captcha
+* Requriements: Python3, selenium, pandas, webdriver_manager
 
---
+++ rbi.py
@@ -0,0 +1,64 @@
+import re, time,os
+import pandas as pd
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = Chrome(service=Service(ChromeDriverManager().install()))
+df = pd.DataFrame() 
+
+# Open website and select all press releases of 2023
+driver.get("https://www.rbi.org.in/Scripts/BS_PressReleaseDisplay.aspx")
+if(re.search("Your support ID is:*",driver.page_source)): input("\n Press enter to cotinue after completing captcha:")
+
+driver.find_element(By.ID,'btn2023').click()
+if(re.search("Your support ID is:*",driver.page_source)): input("\n Press enter to cotinue after completing captcha:")
+
+driver.find_element(By.ID,'20230').click()
+if(re.search("Your support ID is:*",driver.page_source)): input("\n Press enter to cotinue after completing captcha:")
+
+text = driver.page_source
+table = re.search('<table class="tablebg".*</table>',text)
+if(table):
+	tableText = text[table.start():table.end()]
+tableText = tableText.split("</tr>")
+
+
+for text in tableText:
+	prid = re.findall('[0-9]+">91 days, 182 days and 364 days T-Bill Auction Result: Cut off',text)
+	if(len(prid)>0):
+		prid = prid[0].split('"')[0]
+		driver.get("https://www.rbi.org.in/Scripts/BS_PressReleaseDisplay.aspx?prid="+prid)
+		if(re.search("Your support ID is:*",driver.page_source)): input("\n Press enter to cotinue after completing captcha:")
+
+		try:
+			with open("tmp.html", "w", encoding="utf-8") as f:
+			    f.write(driver.page_source)
+
+			dfs = pd.read_html("tmp.html")
+
+			d = dfs[2]
+			returns = [d.loc[2,2],d.loc[2,3],d.loc[2,4]]
+			for i in range(len(returns)):
+				try:
+					returns[i] = ((returns[i].split(":"))[1])[:-1]
+				except:
+					returns[i] = "NA"
+
+			d = dfs[0]
+			date = d.loc[1,0].split(":")[1]
+
+			df1 = pd.DataFrame({"Date":[date], "91 DTB Yield":[returns[0]], "182 DTB Yield":[returns[1]], "364 DTB Yield":[returns[2]]})
+			df = pd.concat((df,df1))
+		except :
+			print("Exception in https://www.rbi.org.in/Scripts/BS_PressReleaseDisplay.aspx?prid="+prid)
+
+driver.quit()
+
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.sort_values(by='Date',ascending=True, ignore_index=True)
+df['Date'] = df['Date'].dt.strftime('%d-%m-%Y')
+df.to_excel("Tbill_Returns_2023.xlsx",sheet_name="Sheet1",index = False)
+
+os.remove("tmp.html")
